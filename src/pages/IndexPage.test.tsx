0e2c7c710cc08180719aa265aec1d7351953f8e9
DohyubLee <hani7304@gmail.com>
2024-03-10 12:52:13 +0900

    메이페인지 테스트 코드

--- src/pages/IndexPage.test.tsx
+++ src/pages/IndexPage.test.tsx
@@ -1,14 +1,38 @@
 import React from "react";
-import { getByText, render, screen } from "@testing-library/react";
+import {
+  act,
+  findAllByRole,
+  getByText,
+  render,
+  screen,
+} from "@testing-library/react";
 import IndexPage from "./IndexPage";
-import { MemoryRouter, BrowserRouter } from "react-router-dom";
+import { MemoryRouter, BrowserRouter, Routes, Route } from "react-router-dom";
+import useStore from "../stores/store";
+import TestPage from "./test/TestPage";
+import userEvent from "@testing-library/user-event";
 
-test("renders learn react link", () => {
-  render(
-    <BrowserRouter>
-      <IndexPage />
-    </BrowserRouter>
-  );
-  const indexPageEl = screen.getByRole("button");
-  //   screen.debug(indexPageEl);
+describe("메인 페이지에서 퀴즈 시작하기 버튼을 클릭하여 퀴즈 시작", () => {
+  test("시작하기 버튼을 클릭했을때 문제풀기 페이지로 잘 이동하는지", async () => {
+    const { currentTime, quizList } = useStore.getState();
+
+    const { getByRole, findAllByRole } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/test/:testId" element={<TestPage />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const startBtnEl = getByRole("button");
+    expect(startBtnEl).toBeInTheDocument();
+    const user = userEvent.setup();
+    await act(async () => {
+      await user.click(startBtnEl);
+    });
+    const quizOptionsEl = await findAllByRole("radio", {}, { timeout: 3000 });
+    // screen.debug();
+    // screen.debug(quizOptionsEl);
+    expect(quizOptionsEl.length).toBeGreaterThan(0);
+  });
 });
